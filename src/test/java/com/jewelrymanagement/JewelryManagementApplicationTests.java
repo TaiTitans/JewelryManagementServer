dde21b72af84b8afd10fe1651dc44f417bf92aab
Phan Phat Tai <taib2014946@student.ctu.edu.vn>
2024-05-19 15:25:21 +0700

    setup project base

---
+++ src/test/java/com/jewelrymanagement/JewelryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewelrymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
