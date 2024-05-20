aa4eed40e85fa8b45b06ee087ad898e8934c3dad
Phan Phat Tai <taib2014946@student.ctu.edu.vn>
2024-05-20 13:43:34 +0700

    fixed env for project

--- src/main/java/com/jewelrymanagement/JewelryManagementApplication.java
+++ src/main/java/com/jewelrymanagement/JewelryManagementApplication.java
@@ -1,5 +1,6 @@
 package com.jewelrymanagement;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JewelryManagementApplication {
 
     public static void main(String[] args) {
+        // Load .env file
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // Set environment variables
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         SpringApplication.run(JewelryManagementApplication.class, args);
     }
 }
