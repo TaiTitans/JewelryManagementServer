5b7762ff4185afd6c76c7b4b30f3cd3f63eab241
Phan Phat Tai <taib2014946@student.ctu.edu.vn>
2024-06-06 21:24:07 +0700

    adding api productdetails

--- src/main/java/com/jewelrymanagement/service/ProductDetailsService.java
+++ src/main/java/com/jewelrymanagement/service/ProductDetailsService.java
@@ -5,8 +5,16 @@ import com.jewelrymanagement.entity.Product;
 import com.jewelrymanagement.entity.ProductDetails;
 import com.jewelrymanagement.repository.ProductDetailsRepository;
 import com.jewelrymanagement.repository.ProductRepository;
+import com.jewelrymanagement.util.StatusResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductDetailsService {
@@ -42,4 +50,66 @@ public class ProductDetailsService {
 
         return productDetails;
     }
+
+    public StatusResponse<ProductDetailsDTO> addProductDetails(ProductDetailsDTO productDetailsDTO) {
+        try{
+            ProductDetails productDetails = convertToEntity(productDetailsDTO);
+            productDetailsRepository.save(productDetails);
+            ProductDetailsDTO productDetailsDTOS = convertToDTO(productDetails);
+            return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Success", "ProductDetails create successfully", productDetailsDTOS);
+        }catch (Exception e){
+            return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Error", "Create error", null);
+        }
+    }
+
+    public StatusResponse<List<ProductDetailsDTO>> findAllProductDetails() {
+        try {
+            List<ProductDetails> productDetailsList = productDetailsRepository.findAll();
+            List<ProductDetailsDTO> productDetailsDTOList = productDetailsList.stream().map(this::convertToDTO).collect(Collectors.toList());
+            return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Success", "Product list retrieved successfully", productDetailsDTOList);
+        } catch (Exception e) {
+            return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Error", "An unexpected error occurred", null);
+        }
+    }
+
+    public StatusResponse<ProductDetailsDTO> getProductDetails(Integer id){
+        try{
+            Optional<ProductDetails> productDetailsOptional = productDetailsRepository.findById(id);
+            if(productDetailsOptional.isPresent()){
+                ProductDetails productDetails = productDetailsOptional.get();
+                ProductDetailsDTO productDetailsDTO = convertToDTO(productDetails);
+                return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Success", "ProductDetails retrieved successfully", productDetailsDTO);
+            }else{
+                return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Error", "ProductDetails not found", null);
+            }
+        }catch (Exception e){
+            return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Error", "An unexpected error occurred", null);
+        }
+    }
+
+    public StatusResponse<ProductDetailsDTO> updateProductDetails(Integer id, ProductDetailsDTO productDetailsDTO) {
+        try{
+            if(productDetailsRepository.existsById(id)){
+                ProductDetails productDetails = convertToEntity(productDetailsDTO);
+                productDetails.setProduct_details_id(id);
+                productDetailsRepository.save(productDetails);
+               ProductDetailsDTO productDetailsDTOSave = convertToDTO(productDetails);
+                return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Success", "ProductDetails update successfully", productDetailsDTOSave);
+            }else{
+                return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Error", "ID ProductDetails exits", null);
+            }
+        }catch (Exception e){
+            return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Error", "An unexpected error occurred", null);
+        }
+    }
+
+    public StatusResponse<ProductDetailsDTO> deteteProductDetails(Integer id){
+        try{
+            productDetailsRepository.deleteById(id);
+            return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Success", "ProductDetails delete successfully", null);
+        }catch (Exception e){
+            return new StatusResponse<>(UUID.randomUUID().toString(), LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME), "Error", "Delete error", null);
+        }
+    }
+
 }
